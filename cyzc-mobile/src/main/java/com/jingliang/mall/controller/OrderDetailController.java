fc25b9883ee05a8a11d3f4710cb93de9e7728a61
15706058532 <15706058532@163.com>
2019-12-15 21:50:45 +0800

    添加再来一单的功能

--- cyzc-mobile/src/main/java/com/jingliang/mall/controller/OrderDetailController.java
+++ cyzc-mobile/src/main/java/com/jingliang/mall/controller/OrderDetailController.java
@@ -1,14 +1,33 @@
 package com.jingliang.mall.controller;
 
+import com.jingliang.mall.common.MallBeanMapper;
+import com.jingliang.mall.common.MallResult;
+import com.jingliang.mall.entity.Buyer;
+import com.jingliang.mall.entity.Cart;
+import com.jingliang.mall.entity.OrderDetail;
+import com.jingliang.mall.req.OrderDetailReq;
+import com.jingliang.mall.resp.OrderDetailResp;
+import com.jingliang.mall.service.CartService;
 import com.jingliang.mall.service.OrderDetailService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * 订单详情表Controller
- * 
+ *
  * @author Zhenfeng Li
  * @version 1.0.0
  * @date 2019-09-26 09:10:24
@@ -18,11 +37,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/front/orderDetail")
 @Slf4j
 public class OrderDetailController {
+    @Value("${session.buyer.key}")
+    private String sessionBuyer;
+    private final OrderDetailService orderDetailService;
+    private final CartService cartService;
 
-	private final OrderDetailService orderDetailService;
+    public OrderDetailController(OrderDetailService orderDetailService, CartService cartService) {
+        this.orderDetailService = orderDetailService;
+        this.cartService = cartService;
+    }
 
-	public OrderDetailController (OrderDetailService orderDetailService) {
-		this.orderDetailService = orderDetailService;
-	}
-
+    /**
+     * 再来一单
+     */
+    @ApiOperation(value = "再来一单")
+    @PostMapping("/again")
+    public MallResult<List<OrderDetailResp>> again(@RequestBody OrderDetailReq orderDetailReq, @ApiIgnore HttpSession session) {
+        if (Objects.isNull(orderDetailReq.getOrderId())) {
+            return MallResult.buildParamFail();
+        }
+        Buyer buyer = (Buyer) session.getAttribute(sessionBuyer);
+        List<OrderDetail> orderDetails = orderDetailService.findByOrderId(orderDetailReq.getOrderId());
+        List<OrderDetailResp> orderDetailResps = new ArrayList<>();
+        for (OrderDetail orderDetail : orderDetails) {
+            Cart cart = new Cart();
+            cart.setBuyerId(buyer.getId());
+            cart.setProductId(orderDetail.getProductId());
+            cart.setProductNum(orderDetail.getProductNum());
+			orderDetailResps.add(MallBeanMapper.map(cartService.save(cart),OrderDetailResp.class));
+		}
+        return MallResult.buildOk(orderDetailResps);
+    }
 }
