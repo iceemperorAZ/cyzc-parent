3d48019947302b22a49990dff0de86c2c8c08bb8
CleanCode <15706058532@163.com>
2020-03-27 19:32:19 +0800

    提交

--- cyzc-service/src/main/java/com/jingliang/mall/repository/DataRepository.java
+++ cyzc-service/src/main/java/com/jingliang/mall/repository/DataRepository.java
@@ -43,19 +43,19 @@ public interface DataRepository extends BaseRepository<Coupon, Long> {
     /**
      * 根据天和区域统计商户
      */
-    @Query(value = "SELECT DATE_FORMAT(b.create_time,'%Y-%m-%d') as datetime ,count(1) as  'day' FROM `tb_buyer` as b JOIN tb_user as u ON b.sale_user_id=u.id JOIN tb_user m_u ON  u.manager_id=m_u.id  WHERE DATE_FORMAT(b.create_time,'%Y-%m-%d')=DATE_FORMAT(:date,'%Y-%m-%d') AND m_u.region=:region GROUP BY DATE_FORMAT(b.create_time,'%Y-%m-%d');", nativeQuery = true)
+    @Query(value = "SELECT  DATE_FORMAT(b.create_time,'%Y-%m-%d') as datetime ,count(1) as  'day' FROM `tb_buyer` as b JOIN tb_user as u ON b.sale_user_id=u.id JOIN tb_manager_sale m_s ON  m_s.sale_id =u.id AND  m_s.create_time<=now() AND m_s.untying_time>=now() JOIN tb_user m_u ON m_s.manager_id=m_u.id WHERE DATE_FORMAT(b.create_time,'%Y-%m-%d')= DATE_FORMAT(:date,'%Y-%m-%d') AND m_u.region=:region   GROUP BY DATE_FORMAT(b.create_time,'%Y-%m-%d')", nativeQuery = true)
     public Map<String,Object>  countAllBuyerByRegionAndDay(Date date, String region);
 
     /**
      * 根据月分和区域统计商户
      */
-    @Query(value = "SELECT count(1) as  'month' FROM `tb_buyer` as b JOIN tb_user as u ON b.sale_user_id=u.id JOIN tb_user m_u ON  u.manager_id=m_u.id  WHERE DATE_FORMAT(b.create_time,'%Y-%m') = DATE_FORMAT(:date,'%Y-%m') AND m_u.region=:region", nativeQuery = true)
+    @Query(value = "SELECT count(1) as  'month' FROM `tb_buyer` as b JOIN tb_user as u ON b.sale_user_id=u.id JOIN tb_manager_sale m_s ON  m_s.sale_id =u.id AND m_s.create_time<=now() AND m_s.untying_time>=now() JOIN tb_user m_u ON m_s.manager_id=m_u.id WHERE DATE_FORMAT(b.create_time,'%Y-%m')= DATE_FORMAT(:date,'%Y-%m') AND m_u.region=:region   GROUP BY DATE_FORMAT(b.create_time,'%Y-%m')", nativeQuery = true)
     public Integer countAllBuyerByRegionAndMonth(Date date, String region);
 
     /**
      * 根据区域统计商户
      */
-    @Query(value = "SELECT count(1) as  tatal FROM `tb_buyer` as b JOIN tb_user as u ON b.sale_user_id=u.id JOIN tb_user m_u ON  u.manager_id=m_u.id  WHERE  m_u.region=:region ;", nativeQuery = true)
+    @Query(value = "SELECT count(1) as  tatal FROM `tb_buyer` as b JOIN tb_user as u ON b.sale_user_id=u.id JOIN tb_manager_sale m_s ON  m_s.sale_id =u.id AND m_s.create_time<=now() AND m_s.untying_time>=now() JOIN tb_user m_u ON m_s.manager_id=m_u.id WHERE  m_u.region=:region ", nativeQuery = true)
     public Integer countAllBuyerByRegion(String region);
 
 }
