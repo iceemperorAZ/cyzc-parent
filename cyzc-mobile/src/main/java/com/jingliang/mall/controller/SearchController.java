2de827814a680dfcde1dbe4f5747627535ed0f0e
CleanCode <15706058532@163.com>
2020-04-02 13:57:47 +0800

    提交

--- cyzc-mobile/src/main/java/com/jingliang/mall/controller/SearchController.java
+++ cyzc-mobile/src/main/java/com/jingliang/mall/controller/SearchController.java
@@ -21,7 +21,7 @@
 //import org.springframework.data.domain.Sort;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController; 
+//import org.springframework.web.bind.annotation.RestController;
 //
 ///**
 // * 搜索Controller
