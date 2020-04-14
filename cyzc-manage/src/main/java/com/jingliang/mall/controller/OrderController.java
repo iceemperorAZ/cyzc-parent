5d49579458beb93852ccfa02188cd2659f9f3496
CleanCode <15706058532@163.com>
2020-04-14 18:22:58 +0800

    订单多选

--- cyzc-manage/src/main/java/com/jingliang/mall/controller/OrderController.java
+++ cyzc-manage/src/main/java/com/jingliang/mall/controller/OrderController.java
@@ -160,7 +160,7 @@ public class OrderController {
      */
     @ApiOperation(value = "分页查询全部用户订单信息")
     @GetMapping("/page/all")
-    public Result<MallPage<OrderResp>> pageAll(OrderReq orderReq) {
+    public Result<MallPage<OrderResp>> pageAll(OrderReq orderReq, @RequestParam(value = "orderStatuses", required = false) List<Integer> orderStatuses) {
         log.debug("请求参数：{}", orderReq);
         PageRequest pageRequest = PageRequest.of(orderReq.getPage(), orderReq.getPageSize());
         if (StringUtils.isNotBlank(orderReq.getClause())) {
@@ -168,6 +168,7 @@ public class OrderController {
         }
         Specification<Order> orderSpecification = (Specification<Order>) (root, query, cb) -> {
             List<Predicate> predicateList = new ArrayList<>();
+            List<Predicate> orOredicateList = new ArrayList<>();
             if (Objects.nonNull(orderReq.getId())) {
                 predicateList.add(cb.equal(root.get("id"), orderReq.getId()));
             }
@@ -180,11 +181,17 @@ public class OrderController {
             if (Objects.nonNull(orderReq.getCreateTimeStart()) && Objects.nonNull(orderReq.getCreateTimeEnd())) {
                 predicateList.add(cb.between(root.get("createTime"), orderReq.getCreateTimeStart(), orderReq.getCreateTimeEnd()));
             }
-            if (Objects.nonNull(orderReq.getOrderStatus())) {
-                predicateList.add(cb.equal(root.get("orderStatus"), orderReq.getOrderStatus()));
+            if (!CollectionUtils.isEmpty(orderStatuses)) {
+                for (Integer orderStatus : orderStatuses) {
+                    orOredicateList.add(cb.equal(root.get("orderStatus"), orderStatus));
+                }
             }
             predicateList.add(cb.equal(root.get("isAvailable"), true));
-            query.where(cb.and(predicateList.toArray(new Predicate[0])));
+            if (!CollectionUtils.isEmpty(orderStatuses)) {
+                query.where(cb.and(predicateList.toArray(new Predicate[0])), cb.or(orOredicateList.toArray(new Predicate[0])));
+            } else {
+                query.where(cb.and(predicateList.toArray(new Predicate[0])));
+            }
             query.orderBy(cb.desc(root.get("createTime")));
             return query.getRestriction();
         };
